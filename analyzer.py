d92591761e392f2d8f6b05439c663a4598951880
jyoshit.ai_enthusiast <venkatjyoshitpotnuru7@gmail.com>
2026-07-22 20:27:09 +0530

    updated gemini model

--- analyzer.py
+++ analyzer.py
@@ -26,7 +26,7 @@ Return ONLY valid JSON.
 """
 
     response = client.models.generate_content(
-        model="gemini-3.5-flash",
+        model="gemini-2.5-flash",
         contents=prompt
     )
 
